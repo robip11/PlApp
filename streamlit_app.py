a0d96c1f4e66befede67438a8ba3a6f91a27e46b
Požonec Róbert <robert.pozonec@stud.uni-obuda.hu>
2024-12-19 01:07:30 +0100

    #

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split, RandomizedSearchCV
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, LabelEncoder
 from datetime import datetime
 from sklearn.ensemble import RandomForestRegressor
 
-player_overview_csv = pd.read_csv('player_overview.csv')
+player_overview_csv = pd.read_csv(r"D:\Dokumentumok\Egyetem\7. félév\Bevezetés a gépi tanulásba\archive\player_overview.csv")
 
 label_encoder = LabelEncoder()
 
@@ -69,13 +67,6 @@ model.fit(X_train, y_train)
 
 # 7. Predikciók a standardizált teszt adatokon
 y_pred = model.predict(X_test)
-                       
-# 8. Kiértékelés
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
-
-print(f"MSE: {mse}")
-print(f"R2: {r2}")
 
 st.title("Player Appearances Prediction")
 st.write("Adja meg a játékos adatait, hogy megkapja a predikált \"Appearances\" értéket!")
